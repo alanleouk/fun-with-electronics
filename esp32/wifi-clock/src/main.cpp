10b55520c285b707b9f24425d2fc3b4a055332cb
unknown <alanleouk@gmail.com>
2024-01-01 22:42:10 +0100

    WIP

--- esp32/wifi-clock/src/main.cpp
+++ esp32/wifi-clock/src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-void setup() {
-  
+#define DHT_SENSOR_PIN 4
+#define DHT_SENSOR_TYPE DHT11
+
+DHT dhtSensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
+void setup()
+{
+  Serial.begin(115200);
+  dhtSensor.begin();
+  delay(2000);
 }
 
-void loop() {
-  
+void loop()
+{
+  float humidity = dhtSensor.readHumidity();
+  float tempC = dhtSensor.readTemperature();
+  float tempF = dhtSensor.readTemperature(true);
+
+  if (isnan(tempC) || isnan(tempF) || isnan(humidity))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+  else
+  {
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%");
+    Serial.print("  |  ");
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C  ~  ");
+    Serial.print(tempF);
+    Serial.println("°F");
+  }
+
+  delay(2000);
 }
